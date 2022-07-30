62783fddcb1069db327631d6a73b00b222b334dd
maciejmar <memgos@gmail.com>
2022-07-30 17:56:33 +0200

    placing project

---
+++ src/main/java/pl/maciejmar/memci/MemciApplication.java
@@ -0,0 +1,13 @@
+package pl.maciejmar.memci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/maciejmar/memci/MemciApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejmar.memci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
